aaab95f1829f915d2fb2b775fbde978fd2de3842
Khanh Nguyen Dev <quckhnh2921@gmail.com>
2025-02-07 10:58:54 +0700

    rename ResolveConnection

--- BE/PRN231/Program.cs
+++ BE/PRN231/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.ResolveServices(connectionString);
+builder.Services.ResolveConnectionString(connectionString);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
--- BE/PRN231/ResolveDependencies/ResolveConnection.cs
+++ BE/PRN231/ResolveDependencies/ResolveConnection.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PRN231.ResolveDependencies
 {
-    public static class Resolve
+    public static class ResolveConnection
     {
-        public static IServiceCollection ResolveServices(this IServiceCollection services, string conn)
+        public static IServiceCollection ResolveConnectionString(this IServiceCollection services, string conn)
         {
             services.AddDbContext<AppDbContext>(option => option.UseSqlServer(conn));
             return services;
