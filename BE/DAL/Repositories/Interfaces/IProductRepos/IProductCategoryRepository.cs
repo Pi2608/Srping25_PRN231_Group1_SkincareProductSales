675f037c71881285f53172f5feac98e645e20572
Khanh Nguyen Dev <quckhnh2921@gmail.com>
2025-02-08 17:17:47 +0700

    remove unuse library

--- BE/DAL/Repositories/Interfaces/IProductRepos/IProductCategoryRepository.cs
+++ BE/DAL/Repositories/Interfaces/IProductRepos/IProductCategoryRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DAL.Repositories.Interfaces.IProductRepos
+﻿namespace DAL.Repositories.Interfaces.IProductRepos
 {
     internal interface IProductCategoryRepository
     {
