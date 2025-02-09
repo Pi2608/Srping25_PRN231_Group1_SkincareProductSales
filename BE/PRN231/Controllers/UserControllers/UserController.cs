205b8a4e54bae289229fd24a3daa4b7b419b2f73
Khanh Nguyen Dev <quckhnh2921@gmail.com>
2025-02-09 11:19:53 +0700

    add  BaseController and UserController for testing

---
+++ BE/PRN231/Controllers/UserControllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Services.Interfaces.IUserServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRN231.Controllers.UserControllers
+{
+    public class UserController : BaseController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUser()
+        {
+            return Ok("Success");
+        }
+    }
+}
