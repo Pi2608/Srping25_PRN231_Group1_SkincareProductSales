d336440d5b3a6a0e2bfefbfbde007852536b2c9b
Pi2608 <ibaxq26@gmail.com>
2025-02-09 19:04:54 +0700

    First UI Commit

---
+++ FE/src/Pages/User/Home/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Banner from '../../../assets/product-img.png';
+import Header from '../../../Components/Header/Header';
+import Footer from '../../../Components/Footer/Footer';
+import Products from './Product/Product';
+import Review from './Review/Review';
+import './Home.css'; 
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header/>
+      
+      <section className="home-banner">
+        <div className='banner-left'>
+          <p>Discover the best skincare products tailored for you.</p>
+          <button className='discover'>Discover</button>
+        </div>
+        <div className="banner-right">
+          <img src={Banner} alt="Banner" />
+        </div>
+      </section>
+      
+      {/* <section className="about-company" id="products">
+        <h2>About Company</h2>
+        <div className="product-list">
+          <div className="product-item">
+            <h3>Product 1</h3>
+            <p>Description of Product 1</p>
+          </div>
+          <div className="product-item">
+            <h3>Product 2</h3>
+            <p>Description of Product 2</p>
+          </div>
+          <div className="product-item">
+            <h3>Product 3</h3>
+            <p>Description of Product 3</p>
+          </div>
+        </div>
+      </section>
+       */}
+
+       <Products/>
+       <Review/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
