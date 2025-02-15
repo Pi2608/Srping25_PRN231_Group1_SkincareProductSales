4941e2ee62be4618b22332c84e8505e9d7ee7a7a
Khanh Nguyen Dev <quckhnh2921@gmail.com>
2025-02-15 16:24:17 +0700

    add order service

--- BE/BLL/Services/Interfaces/IOrderServices/IOrderService.cs
+++ BE/BLL/Services/Interfaces/IOrderServices/IOrderService.cs
@@ -1,6 +1,13 @@
-﻿namespace BLL.Services.Interfaces.IOrderServices
+﻿using DAL.Models.OrderModel;
+
+namespace BLL.Services.Interfaces.IOrderServices
 {
     public interface IOrderService
     {
+        Task<List<Order>> GetAllOrder();
+        Task<Order> GetOrderById(Guid id);
+        Task<Order> CreateOrder(Order order);
+        Task<Order> UpdateOrder(Guid id, Order order);
+        Task<bool> DeleteOrder(Guid id);
     }
 }
--- BE/DAL/Repositories/Interfaces/IRepository.cs
+++ BE/DAL/Repositories/Interfaces/IRepository.cs
@@ -8,7 +8,7 @@ namespace DAL.Repositories.Interfaces
         Task<IQueryable<T>> GetAllAsync(Expression<Func<T, bool>>? expression = null, bool noTracked = false, params string[] includeProperties);
         Task<T> GetWithConditionAsync(Expression<Func<T, bool>>? expression, bool noTracked = false, params string[] includeProperties);
         Task<T> GetByIdAsync(Guid Id, bool noTracked = false, params string[] includeProperties);
-        Task AddAsync(T entity);
+        Task<T> AddAsync(T entity);
         Task AddRangeAsync(IEnumerable<T> values);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity, bool isHardDelete = false);
