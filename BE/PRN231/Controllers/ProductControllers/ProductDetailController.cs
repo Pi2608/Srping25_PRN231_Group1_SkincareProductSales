d90745bc651d5ccf66d531a6bde55d516138bae1
EmBerrrrrr <lan18051973@gmail.com>
2025-03-08 12:55:00 +0700

    ProductDetailService

--- BE/PRN231/Controllers/ProductControllers/ProductDetailController.cs
+++ BE/PRN231/Controllers/ProductControllers/ProductDetailController.cs
@@ -1,6 +1,11 @@
-﻿namespace PRN231.Controllers.ProductControllers
+﻿using BLL.Services.Interfaces.IProductServices;
+using DAL.Models.ProductModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRN231.Controllers.ProductControllers
 {
     public class ProductDetailController : BaseController
     {
+        private readonly IProductService _productDetailService;
     }
 }
