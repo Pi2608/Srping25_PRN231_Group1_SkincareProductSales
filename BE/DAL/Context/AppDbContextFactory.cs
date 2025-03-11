dad81218285212d48d9c286f9a8fce0d5613c48e
kietta <truonganhkietdev@gmail.com>
2025-03-11 21:54:10 +0700

    re setup AppDbContextFactory to get connectionString

--- BE/DAL/Context/AppDbContextFactory.cs
+++ BE/DAL/Context/AppDbContextFactory.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace DAL.Context
 {
@@ -12,8 +13,20 @@ namespace DAL.Context
     {
         public AppDbContext CreateDbContext(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            }
+
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Data Source = KIETTA; Database = PRN231; Trusted_Connection = True; TrustServerCertificate = True");
+            optionsBuilder.UseSqlServer(connectionString);
 
             return new AppDbContext(optionsBuilder.Options);
         }
