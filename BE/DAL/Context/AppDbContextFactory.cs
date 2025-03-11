85be44234ad8f84ff632b2e4903f2461b4b5c5f0
kietta <truonganhkietdev@gmail.com>
2025-03-11 21:50:40 +0700

    add price to Prodcut

---
+++ BE/DAL/Context/AppDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Context
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Data Source = KIETTA; Database = PRN231; Trusted_Connection = True; TrustServerCertificate = True");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
