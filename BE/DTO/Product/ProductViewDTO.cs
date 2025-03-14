cd9bcbc4c9cf3ba92dc557f890a3aaeee0c5e907
EmBerrrrrr <lan18051973@gmail.com>
2025-03-14 22:36:50 +0700

    fix 1 product get many category

--- BE/DTO/Product/ProductViewDTO.cs
+++ BE/DTO/Product/ProductViewDTO.cs
@@ -6,7 +6,12 @@
         public string Name { get; set; }
         public string Image { get; set; }
         public string ShortDescription { get; set; }
-        public List<ProductDetailViewDto> ProductDetails { get; set; }
-        public List<ProductCategoryViewDto> ProductCategories { get; set; }
+        public List<CategoryViewDTO> Categories { get; set; }
+    }
+
+    public class CategoryViewDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 }
